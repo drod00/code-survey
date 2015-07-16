bd821db0556835a85e394232c15ea6b534455e45
david <drod00@me.com>
2015-07-16 18:35:41 +0000

    added a route for results

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,12 @@ Bundler.require
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    erb :index #Comment by David
+    erb:index #Comment by David
   end
-
+  
+  post '/results' do #what i do when i submit a forum with action = to '/results'
+    user_location=params[:location]#params
+    user_city=params[:city]
+  erb:results
+  end
 end
